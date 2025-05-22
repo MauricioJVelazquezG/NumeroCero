4f5ffdf7664f749369d0cdd85025694562104c9a
MauricioJVelazquezG <20243ds142@utez.edu.mx>
2025-05-22 08:46:21 -0600

    Primera prueba del codigo

---
+++ NumeroCero.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class NumeroCero {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String numero = JOptionPane.showInputDialog("Give me a number:");
+        int numero1 = Integer.parseInt(numero);
+
+        while (numero1 == 0) {
+            System.out.println("El numero no puede ser 0 escoge un numero positivo");
+            String numero = JOptionPane.showInputDialog("Give me a number:");
+            int numero1 = Integer.parseInt(numero);
+            
+        }
+    }
+}
